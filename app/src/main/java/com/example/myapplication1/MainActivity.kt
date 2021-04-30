2f8aa9809d900ba4a436ed9ab9cfed47bdc89286
valentynkuts <valik.kuts@gmail.com>
2021-04-30 11:06:36 +0200

    using id instead of ref

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -6,18 +6,23 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+// to have access to IDs as  buttonId, textView ...
+// build.gradle (:app) → apply plugin: 'kotlin-android-extensions'
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get reference to button
-        val btn_click_me = findViewById(R.id.button) as Button    // Alt+Shift+Enter to import class
+        // get reference to button and says that type is Button
+        //val btnClickMe = findViewById(R.id.buttonId) as Button    // Alt+Shift+Enter to import class
         // get reference to text
-        var myTextView = findViewById(R.id.textView) as TextView
+        //var myTextView = findViewById(R.id.textView) as TextView
         var timesClicking = 0
         // set on-click listener
-        btn_click_me.setOnClickListener {
+        /*
+        btnClickMe.setOnClickListener {
             // to change text after clicking
             //myTextView.text = "Test"
             timesClicking ++
@@ -26,6 +31,21 @@ class MainActivity : AppCompatActivity() {
             // to see message 'Hi, user' after clicking
             Toast.makeText(this@MainActivity, "Hi, User.", Toast.LENGTH_SHORT).show()
         }
+         */
+
+        // or it can be replace by id of the button
+
+        buttonId.setOnClickListener {
+            // to change text after clicking
+            //myTextView.text = "Test"
+            timesClicking ++
+            textView.text = timesClicking.toString()
+
+            // to see message 'Hi, user' after clicking
+            Toast.makeText(this@MainActivity, "Hi, User.", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
 }
